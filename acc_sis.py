24057a8bec113c6d35220e1efa536f2b14dcd35b
krishnan <metarus208@gmail.com>
2015-09-28 00:10:24 -0400

    Working I

--- acc_sis.py
+++ acc_sis.py
@@ -48,34 +48,36 @@ for imm in immunized:
 
 print "Status: ", status_list
 
-for i in xrange(max_time):
-  cnt1 = 0
-  cnt2 = 0 
-  print "Iteration " + str(i)
-  for node in N:
+with open("acc_sis_results.csv","w+") as f:
+  for i in xrange(max_time):
+    cnt1 = 0
+    cnt2 = 0 
+    print "Iteration " + str(i)
+    for node in N:
 
-    # For infected nodes, recover with
-    # probability gamma
-    #pdb.set_trace()
-    if status_list[node] == True:
-      if random.random() <= g:
-        status_list[node] = False
+      # For infected nodes, recover with
+      # probability gamma
+      #pdb.set_trace()
+      if status_list[node] == True:
+        if random.random() <= g:
+          status_list[node] = False
 
-    # For healthy nodes, get infected by each infected 
-    # neighbor with probability b
-    elif status_list[node] == False:
-      for neighbor in all_neighbors(G,node):
-        if random.random() <= b:
-          status_list[node] = True
-    else:
+      # For healthy nodes, get infected by each infected 
+      # neighbor with probability b
+      elif status_list[node] == False:
+        for neighbor in all_neighbors(G,node):
+          if random.random() <= b:
+            status_list[node] = True
+      else:
         print "Node is IMMUNIZED"
 
-  for status in status_list:
-    if status == True:
-      cnt1 += 1
-    else:
-      cnt2 += 1
-  print "No. of Infected nodes: ", cnt1
-  print "No. of healthy nodes: ", cnt2
+    for status in status_list:
+      if status == True:
+        cnt1 += 1
+      else:
+        cnt2 += 1
+    print "No. of Infected nodes: ", cnt1
+    print "No. of healthy nodes: ", cnt2
+    f.write(str(i+1) + "," + str(cnt1) + "," + str(cnt2) + "\n")
   
 print status_list
--- sis.py
+++ sis.py
@@ -22,33 +22,34 @@ G = erdos_renyi_graph(n,p)
 N = G.nodes()
 status_list = [True] * n;
 
+with open("sis_results.csv","w+") as f:
+  for i in xrange(max_time):
+    cnt1 = 0
+    cnt2 = 0 
+    print "Iteration " + str(i)
+    for node in N:
 
-for i in xrange(max_time):
-  cnt1 = 0
-  cnt2 = 0 
-  print "Iteration " + str(i)
-  for node in N:
+      # For infected nodes, recover with
+      # probability gamma
+      if status_list[node]:
+        if random.random() <= g:
+          status_list[node] = False
 
-    # For infected nodes, recover with
-    # probability gamma
-    if status_list[node]:
-      if random.random() <= g:
-        status_list[node] = False
+      # For healthy nodes, get infected by each infected 
+      # neighbor with probability b
+      else:
+        for neighbor in all_neighbors(G,node):
+          if random.random() <= b:
+            status_list[node] = True
 
-    # For healthy nodes, get infected by each infected 
-    # neighbor with probability b
-    else:
-      for neighbor in all_neighbors(G,node):
-        if random.random() <= b:
-          status_list[node] = True
-
-  for status in status_list:
-    if status:
-      cnt1 += 1
-    else:
-      cnt2 += 1
-  print "No. of Infected nodes: ", cnt1
-  print "No. of healthy nodes: ", cnt2
+    for status in status_list:
+      if status == True:
+        cnt1 += 1
+      else:
+        cnt2 += 1
+    print "No. of Infected nodes: ", cnt1
+    print "No. of healthy nodes: ", cnt2
+    f.write(str(i+1) + "," + str(cnt1) + "," + str(cnt2) + "\n")
   
 print status_list
 
